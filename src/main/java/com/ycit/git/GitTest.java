5931727890034826616f3e2297a8b0175bd8d20b
spc <1634798770@qq.com>
2021-10-17 11:55:12 +0800

    first commit

---
+++ src/main/java/com/ycit/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.ycit.git;
+
+/**
+ * @Auther: spc
+ * @Date: 2021/10/17
+ * @Description: com.ycit.git
+ * @version: 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
